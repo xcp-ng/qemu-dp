5cec0456927de046e36b6c16bb3db280eee46494
Vates Git Importer <gitimporter@vates.tech>
2015-09-19 08:09:42 +0200

    When allocating the VRAM, QEMU allocates it immediately after RAM. This mostly works fine as there is a gap between the end of RAM and the start of the MMIO region.  However, for instance when QEMU has 2G and the MMIO region is 2G, there is no gap and QEMU will allocate the VRAM at 2G which causes breakage since a BAR may be mapped to the same location.
    Force RAM block address for guest memory allocation to always point to
    a reserved area in guest address space.

--- include/hw/xen/xen.h
+++ include/hw/xen/xen.h
@@ -42,13 +42,15 @@ void xenstore_store_pv_console_info(int i, struct Chardev *chr);
 
 void xen_hvm_init(PCMachineState *pcms, MemoryRegion **ram_memory);
 
-void xen_ram_alloc(ram_addr_t ram_addr, ram_addr_t size,
-                   struct MemoryRegion *mr, Error **errp);
+ram_addr_t xen_ram_alloc(ram_addr_t ram_addr, ram_addr_t size,
+                         struct MemoryRegion *mr, Error **errp);
 void xen_hvm_modified_memory(ram_addr_t start, ram_addr_t length);
 
 void xen_register_framebuffer(struct MemoryRegion *mr);
 
 void xen_unassigned_access(hwaddr addr, bool is_write, void *val,
                            unsigned size);
+
+#define VRAM_RESERVED_ADDRESS  0xff000000
 
 #endif /* QEMU_HW_XEN_H */
