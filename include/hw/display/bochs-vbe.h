933ea3b3aec5b8b93d9bc7b1038357e458ec815d
Igor Druzhinin <igor.druzhinin@citrix.com>
2017-04-26 11:55:55 +0100

    CP-21434 Implement VBE LFB physical address register
    QEMU-trad implements additional VBE registers for getting LFB
    physical address inside VGA BIOS code. QEMU-upstream doesn't
    have these registers implemented and return 0 when an HVM guest is
    trying to access them from ROMBIOS code. This eventually leads to
    a triple fault inside the guest.
    
    A normal way to fix it would be to update the VGA BIOS code inside
    ROMBIOS and use PCI BAR access to get the LFB address. Unfortunately,
    Xen migrates the ROMBIOS code region along with any other domain
    memory that means we cannot migrate an old guest to the new QEMU.
    
    Signed-off-by: Igor Druzhinin <igor.druzhinin@citrix.com>

--- include/hw/display/bochs-vbe.h
+++ include/hw/display/bochs-vbe.h
@@ -21,6 +21,7 @@
 #define VBE_DISPI_INDEX_Y_OFFSET        0x9
 #define VBE_DISPI_INDEX_NB              0xa /* size of vbe_regs[] */
 #define VBE_DISPI_INDEX_VIDEO_MEMORY_64K 0xa /* read-only, not in vbe_regs */
+#define VBE_DISPI_INDEX_LFB_ADDRESS_H    0xb /* read-only, trad compat */
 
 /* VBE_DISPI_INDEX_ID */
 #define VBE_DISPI_ID0                   0xB0C0
