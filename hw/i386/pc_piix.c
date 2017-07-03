cdbf3160c2ad0945abd1ea5e6f02c1a2c90eef9a
Ross Lagerwall <ross.lagerwall@citrix.com>
2017-07-03 13:56:36 +0100

    hotplug: Implement legacy CPU hot unplug
    This is done in the same way that qemu-trad does CPU hot unplug.
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>

--- hw/i386/pc_piix.c
+++ hw/i386/pc_piix.c
@@ -970,8 +970,11 @@ DEFINE_I440FX_MACHINE(v0_11, "pc-0.11", pc_compat_0_13,
 
 static void pc_i440fx_0_10_machine_options(MachineClass *m)
 {
+    HotplugHandlerClass *hc = HOTPLUG_HANDLER_CLASS(m);
+
     pc_i440fx_0_11_machine_options(m);
     m->hw_version = "0.10";
+    hc->unplug_request = NULL;
     SET_MACHINE_COMPAT(m, PC_COMPAT_0_10);
 }
 
