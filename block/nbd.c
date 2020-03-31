4fccc1ff3b220ca2fb8d406fa51695b0508209ab
ben.sims@citrix.com <ben.sims@citrix.com>
2020-03-31 08:38:15 +0000

    CP-33348 - Allow media replace qmp command to take a numeric socket fds for nbd.

--- block/nbd.c
+++ block/nbd.c
@@ -192,10 +192,13 @@ static void nbd_parse_filename(const char *filename, QDict *options,
         goto out;
     }
 
-    /* are we a UNIX or TCP socket? */
+    qdict_put_str(options, "server.uri", host_spec);
+    /* are we a UNIX or TCP socket, or a preopened socket? */
     if (strstart(host_spec, "unix:", &unixpath)) {
         qdict_put_str(options, "server.type", "unix");
         qdict_put_str(options, "server.path", unixpath);
+    } else if (strstart(host_spec, "fd:", &unixpath)) {
+        qdict_put_str(options, "server.type", "fd");
     } else {
         InetSocketAddress *addr = g_new(InetSocketAddress, 1);
 
@@ -262,40 +265,20 @@ static SocketAddress *nbd_config(BDRVNBDState *s, QDict *options,
 {
     SocketAddress *saddr = NULL;
     QDict *addr = NULL;
-    QObject *crumpled_addr = NULL;
-    Visitor *iv = NULL;
-    Error *local_err = NULL;
-
+    const char *uri;
+    /* 
+     * Important to drain the QDict options as block.c checks to see
+     * if it is empty on return.
+     */
     qdict_extract_subqdict(options, &addr, "server.");
     if (!qdict_size(addr)) {
         error_setg(errp, "NBD server address missing");
         goto done;
     }
-
-    crumpled_addr = qdict_crumple(addr, errp);
-    if (!crumpled_addr) {
-        goto done;
-    }
-
-    /*
-     * FIXME .numeric, .to, .ipv4 or .ipv6 don't work with -drive
-     * server.type=inet.  .to doesn't matter, it's ignored anyway.
-     * That's because when @options come from -blockdev or
-     * blockdev_add, members are typed according to the QAPI schema,
-     * but when they come from -drive, they're all QString.  The
-     * visitor expects the former.
-     */
-    iv = qobject_input_visitor_new(crumpled_addr);
-    visit_type_SocketAddress(iv, NULL, &saddr, &local_err);
-    if (local_err) {
-        error_propagate(errp, local_err);
-        goto done;
-    }
-
+    uri = qdict_get_str(addr, "uri");
+    saddr = socket_parse(uri, errp);
 done:
     QDECREF(addr);
-    qobject_decref(crumpled_addr);
-    visit_free(iv);
     return saddr;
 }
 
