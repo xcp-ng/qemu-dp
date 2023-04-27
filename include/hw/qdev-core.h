cc954028cc6f39eb034749c6c4700da708f76cc9
Alexander Bulekov <alxndr@bu.edu>
2023-04-27 17:10:06 -0400

    memory: prevent dma-reentracy issues
    Add a flag to the DeviceState, when a device is engaged in PIO/MMIO/DMA.
    This flag is set/checked prior to calling a device's MemoryRegion
    handlers, and set when device code initiates DMA.  The purpose of this
    flag is to prevent two types of DMA-based reentrancy issues:
    
    1.) mmio -> dma -> mmio case
    2.) bh -> dma write -> mmio case
    
    These issues have led to problems such as stack-exhaustion and
    use-after-frees.
    
    Summary of the problem from Peter Maydell:
    https://lore.kernel.org/qemu-devel/CAFEAcA_23vc7hE3iaM-JVA6W38LK4hJoWae5KcknhPRD5fPBZA@mail.gmail.com
    
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/62
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/540
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/541
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/556
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/557
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/827
    Resolves: https://gitlab.com/qemu-project/qemu/-/issues/1282
    Resolves: CVE-2023-0330
    
    Signed-off-by: Alexander Bulekov <alxndr@bu.edu>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Message-Id: <20230427211013.2994127-2-alxndr@bu.edu>
    [thuth: Replace warn_report() with warn_report_once()]
    Signed-off-by: Thomas Huth <thuth@redhat.com>
    (cherry picked from commit a2e1753b8054344f32cf94f31c6399a58794a380)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>
    
    XCP-ng backport
    Backported-by: Thierry Escande <thierry.escande@vates.tech>
    Origin: https://gitlab.com/qemu-project/qemu/-/commit/c40ca2301c7603524eaddb5308a3f524c6f89d24
    Notes:
    - Use of the error code MEMTX_ERROR instead of MEMTX_ACCESS_ERROR (not available
      in Qemu 4.2.1) when a DMA-based reentrancy is detected.

--- include/hw/qdev-core.h
+++ include/hw/qdev-core.h
@@ -138,6 +138,10 @@ struct NamedGPIOList {
     QLIST_ENTRY(NamedGPIOList) node;
 };
 
+typedef struct {
+    bool engaged_in_io;
+} MemReentrancyGuard;
+
 /**
  * DeviceState:
  * @realized: Indicates whether the device has been fully constructed.
@@ -163,6 +167,9 @@ struct DeviceState {
     int num_child_bus;
     int instance_id_alias;
     int alias_required_for_version;
+
+    /* Is the device currently in mmio/pio/dma? Used to prevent re-entrancy */
+    MemReentrancyGuard mem_reentrancy_guard;
 };
 
 struct DeviceListener {
